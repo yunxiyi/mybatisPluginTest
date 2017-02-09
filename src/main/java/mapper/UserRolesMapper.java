5564aa57467ba6a9ad2a3d6cfe08325709a16b3b
yunxiyi <yunxiyi@outlook.com>
2017-02-09 17:25:44 +0800

    init project

---
+++ src/main/java/mapper/UserRolesMapper.java
@@ -0,0 +1,30 @@
+package mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import pojo.UserRoles;
+import pojo.UserRolesExample;
+
+public interface UserRolesMapper {
+    int countByExample(UserRolesExample example);
+
+    int deleteByExample(UserRolesExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UserRoles record);
+
+    int insertSelective(UserRoles record);
+
+    List<UserRoles> selectByExample(UserRolesExample example);
+
+    UserRoles selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UserRoles record, @Param("example") UserRolesExample example);
+
+    int updateByExample(@Param("record") UserRoles record, @Param("example") UserRolesExample example);
+
+    int updateByPrimaryKeySelective(UserRoles record);
+
+    int updateByPrimaryKey(UserRoles record);
+}
---
+++ src/main/java/mapper/UsersMapper.java
@@ -0,0 +1,30 @@
+package mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import pojo.Users;
+import pojo.UsersExample;
+
+public interface UsersMapper {
+    int countByExample(UsersExample example);
+
+    int deleteByExample(UsersExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Users record);
+
+    int insertSelective(Users record);
+
+    List<Users> selectByExample(UsersExample example);
+
+    Users selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Users record, @Param("example") UsersExample example);
+
+    int updateByExample(@Param("record") Users record, @Param("example") UsersExample example);
+
+    int updateByPrimaryKeySelective(Users record);
+
+    int updateByPrimaryKey(Users record);
+}
